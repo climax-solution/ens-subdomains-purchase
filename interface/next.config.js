22442e1f5aa4764ddd792f59460717fb94488ea5
climax-solution <blocklimax@gmail.com>
2022-06-15 10:33:45 +0300

    add env

--- interface/next.config.js
+++ interface/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    mongodburl: "mongodb://localhost:27017/ens-domains",
+    ENSDomain: "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
+    Registrar: "0x90a62e3E954b38038d4ef3321C70465c48ed480B"
+  }
 }
 
 module.exports = nextConfig
