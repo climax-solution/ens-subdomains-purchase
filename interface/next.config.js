a5bca1565142d4c16b04205948b2693f64b5c193
climax-solution <blocklimax@gmail.com>
2022-06-16 15:33:31 +0300

    change registrar contract address

--- interface/next.config.js
+++ interface/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     mongodburl: "mongodb://localhost:27017/ens-domains",
     ENSDomain: "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
-    Registrar: "0x08e0a3ef8aACCb09E92e9A35894Defb4A9143254"
+    Registrar: "0xd5e1b16d2619049E8a8feeC2557026adD996b672"
   }
 }
 
