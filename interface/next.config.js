f58ea86d105a9459937947e6774d032b7ece5317
climax-solution <blocklimax@gmail.com>
2022-06-16 23:18:07 +0300

    update contract address and abi code

--- interface/next.config.js
+++ interface/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     mongodburl: "mongodb://localhost:27017/ens-domains",
     ENSDomain: "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
-    Registrar: "0xd5e1b16d2619049E8a8feeC2557026adD996b672"
+    Registrar: "0xA84bd888B2fe9b9edA835f59EeC31dBD4677F7DA"
   }
 }
 
