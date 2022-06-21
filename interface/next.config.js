9a2364d5c2cb4402b3e34249338ffe924f355210
climax-solution <blocklimax@gmail.com>
2022-06-21 22:38:41 +0300

    update smart contract data

--- interface/next.config.js
+++ interface/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     mongodburl: "mongodb://localhost:27017/ens-domains",
     ENSDomain: "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
-    Registrar: "0xdDa103cB31348AA6852f7fb1e03411C11b966676",
+    Registrar: "0xeFdCFd6bdac10c3Da104d4a62E8D10D01E2e867D",
     ENSRegistry: "0x00000000000c2e074ec69a0dfb2997ba6c7d2e1e"
   }
 }
