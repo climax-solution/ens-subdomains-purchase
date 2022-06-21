e673b25e39ca86489ca2c6a821ec971f009fb5d0
climax-solution <blocklimax@gmail.com>
2022-06-21 20:49:48 +0300

    create empty component

---
+++ interface/components/empty/index.js
@@ -0,0 +1,11 @@
+const Empty = () => (
+    <div
+        className="flex items-center justify-center
+        border border-gray-300 rounded-lg h-52 p-16
+        w-1/2 mx-auto shadow-lg absolute -translate-y-1/2 -translate-x-1/2 left-2/4 top-2/4"
+    >
+        <h3 className="text-gray-300 text-lg font-bold">No items to display.</h3>
+    </div>
+)
+
+export default Empty;
