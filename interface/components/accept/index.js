467c0f2a9512fb9cd6811af0afa8dfdc8af01b0b
climax-solution <blocklimax@gmail.com>
2022-06-23 18:24:21 +0300

    add accepted address

---
+++ interface/components/accept/index.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'react';
+import { NotificationManager} from 'react-notifications';
+import { useAppContext } from "../../context/state";
+
+const Accept = ({ data, update, startLoading }) => {
+
+    const { WEB3, account, registrarContract } = useAppContext();
+    const [isLoading, setLoading] = useState(true);
+    const [isAble, setAble] = useState(false);
+    useEffect(() => {
+        async function checkAble() {
+            setLoading(true);
+            const domain_label = WEB3.utils.sha3(data.domain);
+            const state = await registrarContract.methods.getReserveIndex(domain_label, data.subdomain).call();
+            if (lowercase(account) == lowercase(state.owner)) setAble(true);
+            setLoading(false);
+        }
+        if (registrarContract) {
+            checkAble();
+        }
+    }, [data, registrarContract])
+
+    const removeSubDomain = async() => {
+        startLoading(true);
+
+        try {
+            const { timestamp } = await WEB3.eth.getBlock('latest');
+            const domain_label = WEB3.utils.sha3(data.domain);
+            const state = await registrarContract.methods.getReserveIndex(domain_label, data.subdomain).call();
+            if (state.expiration > timestamp && lowercase(account) == lowercase(state.owner) || state.expiration < timestamp) {
+                await registrarContract.methods.removeSubdomain(domain_label, data.subdomain).send({
+                    from: account
+                });
+                NotificationManager.success("Success");
+            } else {
+                NotificationManager.warning("Not available");
+            }
+
+        } catch(err) {
+
+        }
+        startLoading(false);
+        update();
+    }
+
+    const lowercase = (str) => {
+        return str.toLowerCase();
+    }
+
+    return (
+        <>
+            {
+                isLoading ? (
+                    <div
+                        className="flex w-full items-center justify-between rounded-lg bg-white px-4 py-2 text-left text-sm font-medium text-purple-900 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 cursor-pointer py-7"
+                    >
+                        <div className='flex gap-8 items-center w-full animate-pulse'>
+                            <span className="bg-blue-300 h-4 w-11/12 rounded-lg"></span>
+                            <span className="bg-blue-300 h-4 w-11/12 rounded-lg"></span>
+                            <span className="bg-blue-300 h-4 w-11/12 rounded-lg"></span>
+                            <span className="bg-blue-300 h-4 w-11/12 rounded-lg"></span>
+                        </div>
+                    </div>
+                ) :
+                <>
+                    {
+                        !Object.keys(data).length ? <></>
+                        :
+                        <div
+                            className="flex w-full items-center justify-between rounded-lg bg-white px-4 py-2 text-left text-sm font-medium text-purple-900 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75 cursor-pointer h-20"
+                        >
+                            <div className='flex gap-8 items-center'>
+                                <span className="text-xl font-bold">{data.subdomain}</span>
+                                <span>{data.domain}.eth</span>
+                                <span>{data.owner}</span>
+                                <span>{ WEB3.utils.fromWei(data.price)} ETH</span>
+                            </div>
+                            { isAble && <div className='flex gap-3'>
+                                <button
+                                    className="p-5 border-2 border-rose-200 hover:border-rose-400 rounded-full"
+                                    onClick={removeSubDomain}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg>
+                                </button>
+                            </div> }
+                        </div>
+                    }
+                </>
+            }
+        </>
+    )
+}
+
+export default Accept;
---
+++ interface/pages/accepted/[tab].js
@@ -0,0 +1,68 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import Accept from '../../components/accept';
+import Empty from '../../components/empty';
+import Loader from '../../components/loader';
+import { useAppContext } from "../../context/state";
+
+const Accepted = () => {
+    const { query } = useRouter();
+    const { tab } = query;
+
+    const { registrarContract, account } = useAppContext();
+    const [list, setList] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [isUpdate, setUpdate] = useState(true);
+
+    useEffect(() => {
+        if (registrarContract) {
+            setLoading(true);
+            try {
+                registrarContract.getPastEvents('NewRegistration', {
+                    filter: { owner: account },
+                    fromBlock: 10892100,
+                    toBlock: 'latest'
+                }, function(error, events) {
+                    if (!error) {
+                        if (tab == 'reserve') {
+                            events = events.filter(({returnValues: item}) => item.reserver.toLowerCase() == account.toLowerCase());
+                        }
+                        else if (tab == 'request') {
+                            events = events.filter(({returnValues: item}) => item.owner.toLowerCase() == account.toLowerCase());
+                        }
+                        else events = [];
+                        setList(events);
+                    }
+                })
+            } catch(ess) {
+                console.log(ess);
+            }
+            setLoading(false);
+        }
+    }, [registrarContract, tab, isUpdate]);
+
+    return (
+        <div className='p-4 flex flex-col gap-3'>
+            {
+                isLoading ? <Loader/>
+                : <>
+                    {
+                        list.map((item, idx) => (
+                            <Accept
+                                data={item.returnValues}
+                                key={idx}
+                                update={() => setUpdate(!isUpdate)}
+                                startLoading={setLoading}
+                            />
+                        ))
+                    }
+                    {
+                        !list.length && <Empty/>
+                    }
+                </>
+            }
+        </div>
+    )
+}
+
+export default Accepted;
