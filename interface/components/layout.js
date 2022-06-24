2211ba57c9ba4919bb5babba2a61dc1b8dc448de
climax-solution <blocklimax@gmail.com>
2022-06-25 00:11:26 +0300

    change icon

--- interface/components/layout.js
+++ interface/components/layout.js
@@ -182,10 +182,8 @@ function Layout({ children }) {
                             </a>
                         </li>
                         <li className="cursor-pointer text-white pt-5 pb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 icon icon-tabler icon-tabler-messages" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                                <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
                         </li>
                         <li className="cursor-pointer text-white pt-5 pb-3">
