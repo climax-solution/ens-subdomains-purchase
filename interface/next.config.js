eb552d7bfaef16c697d41ecf4373c5a66c4162e3
climax-solution <blocklimax@gmail.com>
2022-07-01 22:58:16 +0300

    remove mongodb url and add backend

--- interface/next.config.js
+++ interface/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    mongodburl: "mongodb://localhost:27017/ens-domains",
+    backend: "http://142.11.213.249:3000",
     ENSDomain: "0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85",
     Registrar: "0xEd508D6c47644cCbBfe599B070dC4D0eA333dE19",
     ENSRegistry: "0x00000000000c2e074ec69a0dfb2997ba6c7d2e1e"
