6e9dcd5109b2931a04383df43813eb4c38972b7e
climax-solution <blocklimax@gmail.com>
2022-07-01 18:36:07 +0300

    add filter

--- interface/components/user/index.js
+++ interface/components/user/index.js
@@ -1,12 +1,29 @@
 import { SearchIcon, UserIcon } from "@heroicons/react/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import Star from "../review/star";
+import axios from "axios";
 
 const User = ({ address }) => {
 
+    const [star, setStar] = useState(0);
+    const [isLoading, setLoading] = useState(true);
     useEffect(() => {
-
-    });
+        const fetchReviews = async() => {
+            setLoading(true);
+            const _list = await axios.post(`${process.env.backend}/reviews/get-list`, { address }).then(res => {
+                const { list } = res.data;
+                return list;
+            }).catch(err => {
+                return [];
+            });
+            let star_sum = 0;
+            _list.map(item => star_sum += parseInt(item.star) /_list.length );
+            setStar(star_sum);
+            setLoading(false);
+        }
+        fetchReviews();
+    }, [address]);
 
     const shorten = (str, cut = 4) => {
         if (str) {
@@ -17,16 +34,16 @@ const User = ({ address }) => {
     }
 
     return (
-        <div className="w-52 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-3">
-            <div className="flex flex-col items-center pb-10">
+        <div className="w-52 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 px-3">
+            <div className="flex flex-col items-center pb-10 mt-10">
                 <UserIcon className="mb-3 w-24 h-24 rounded-full border-2 p-2"/>
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{shorten(address)}</h5>
                 <div className="flex items-center">
-                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
-                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
-                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
-                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
-                    <svg className="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
+                    {
+                        isLoading ? <span className="animate-pulse w-32 h-4 bg-gray-400 rounded-md inline-block"/>
+                        : <Star gold={star}/>
+                    }
+                    
                 </div>
                 <div className="flex mt-4 space-x-3 lg:mt-6">
                     <Link href={`/review/${address}`}>
--- interface/pages/users/index.js
+++ interface/pages/users/index.js
@@ -1,22 +1,74 @@
 import axios from "axios";
 import User from "../../components/user";
 import Empty from "../../components/empty";
+import { Combobox, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useState } from "react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/outline";
+
+const people = [
+    { id: 1, name: 'Latest', isLatest: true },
+    { id: 2, name: 'Oldest', isLatest: false },
+]
 
 const Users = ({ users }) => {
+
+    const [selected, setSelected] = useState(people[0])
+    const [query, setQuery] = useState('')
+    const [walletAddress, setWalletAddress] = useState('');
+    const [list, setList] = useState(users);
+
+    const filteredPeople =
+      query === ''
+        ? people
+        : people.filter((person) =>
+            person.name
+              .toLowerCase()
+              .replace(/\s+/g, '')
+              .includes(query.toLowerCase().replace(/\s+/g, ''))
+    )
+
+    useEffect(() => {
+        filterUser();
+    }, [walletAddress]);
+
+    const filterUser = () => {
+        if (walletAddress) {
+            const filtered = users.filter(item => (normalize(item.address)).indexOf(normalize(walletAddress)) > -1);
+            setList(filtered);
+        }
+        else setList(users);
+    }
+
+    const normalize = (str) => {
+        return str.toLowerCase();
+    }
+
     return (
-        <div className="flex flex-wrap gap-3 p-4">
-            {
-                users.map((item, idx) => (
-                    <User
-                        key={idx}
-                        address={item.address}
-                    />
-                ))
-            }
-            {
-                !users && <Empty/>
-            }
+        <div>
+            <div className="flex flex-wrap gap-2 justify-center items-center mt-6">
+                <input
+                    type="text"
+                    className="shadow-md border border-violet-300 text-white outline-0 py-2 px-4 rounded-full bg-transparent w-96"
+                    placeholder="Search User...."
+                    value={walletAddress}
+                    onChange={(e) => setWalletAddress(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-wrap gap-3 p-4">
+                {
+                    list.map((item, idx) => (
+                        <User
+                            key={idx}
+                            address={item.address}
+                        />
+                    ))
+                }
+                {
+                    !list.length && <Empty/>
+                }
+            </div>
         </div>
+        
     )
 }
 
